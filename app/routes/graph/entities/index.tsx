2f6095cfca7d7ae48bb64139d5228b906a1925d1
Christopher Möller <chrtze@gmail.com>
2023-01-23 11:58:22 +0100

    fix(reactflow): import react flow styles

--- app/routes/graph/entities/index.tsx
+++ app/routes/graph/entities/index.tsx
@@ -1,5 +1,15 @@
+import reactFlowStyles from 'reactflow/dist/style.css';
+import styles from '~/styles/forceLayout.css';
+
 export default function EntityIndex() {
   return (
     <h1>Index Filler</h1>
   );
+}
+
+export function links() {
+  return [
+    { rel: 'stylesheet', href: reactFlowStyles },
+    { rel: 'stylesheet', href: styles },
+  ];
 }
